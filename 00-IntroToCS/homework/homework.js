40a5c07196df0a2331b5790cf203b31dd9c28802
NAUTA2022 <andresquinteros2017@gmail.com>
2022-04-25 16:30:23 -0300

    v2

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,11 +1,22 @@
 "use strict";
 
 function BinarioADecimal(num) {
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    result = result + num[i] * 2 ** (num.length - 1 - i);
+  }
+  return result;
   // tu codigo aca
   // algo
 }
 
 function DecimalABinario(num) {
+  var result = "";
+  while (num != 0) {
+    result = (num % 2) + result;
+    num = Math.trunc(num / 2);
+  }
+  return result;
   // tu codigo aca
 }
 
